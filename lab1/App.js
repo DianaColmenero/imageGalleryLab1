8fdd25af0283e4a8e3f4b8ab710502cd7c47e87f
Roquecito <rrueda@itexico.net>
2018-10-04 17:04:24 -0500

    adding image element to view

--- lab1/App.js
+++ lab1/App.js
@@ -7,24 +7,19 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {StyleSheet, View, Image} from 'react-native';
 
 type Props = {};
+
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-        <Text style={styles.instructions}>'Hola Diana como estas?'</Text>
+        <Image 
+          style={styles.imageView}
+          source={
+            { uri: 'https://static1.squarespace.com/static/572acd717c65e426e2b580d9/t/5b467f2d758d46a136f18dbd/1531346739681/themarias.jpg' }
+          } />
       </View>
     );
   }
@@ -37,14 +32,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  imageView: {
+    width: 350, height: 350
+  }
 });
